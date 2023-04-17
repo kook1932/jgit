a90dc15c137a0b2bafa71c3597cd62e28ee29bf4
JeongYong <jeongyong.han@does.kr>
2023-04-17 23:38:46 +0900

    Initial commit

---
+++ src/test/java/com/jy/jgit/JgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.jgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
