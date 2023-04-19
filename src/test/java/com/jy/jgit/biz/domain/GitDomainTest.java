28f797f7998b30f0a134f8c099e49f4408555798
JeongYong <jeongyong.han@does.kr>
2023-04-19 23:12:35 +0900

    local commit

--- src/test/java/com/jy/jgit/biz/domain/GitDomainTest.java
+++ src/test/java/com/jy/jgit/biz/domain/GitDomainTest.java
@@ -1,7 +1,10 @@
 package com.jy.jgit.biz.domain;
 
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,14 +15,28 @@ class GitDomainTest {
 	void gitPushTest() {
 		GitDomain git = GitDomain.builder()
 				.username("kook1932")
-				.userToken("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")
+				.userToken("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")
 				.dirPath("C:\\Projects\\jgit")
 				.repoName("origin")
 				.branchName("master")
 				.build();
 
 		git.push();
+	}
 
+	@DisplayName("List all commits in a repository")
+	@Test
+	void getList() {
+		GitDomain git = GitDomain.builder()
+				.username("kook1932")
+				.userToken("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")
+				.dirPath("C:\\Projects\\jgit")
+				.repoName("origin")
+				.branchName("master")
+				.build();
+
+		List<RevCommit> commitList = git.getCommitList();
+		commitList.forEach(c -> System.out.println("commit : " + c));
 	}
 
 }
